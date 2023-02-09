22148a23a0b9922d3579228f1a0cb274b79a0794
expeditus303 <ricardotonietto@gmail.com>
2023-02-09 16:39:22 -0300

    feat: get customers

---
+++ src/app.js
@@ -0,0 +1,63 @@
+import express from "express";
+import dotenv from "dotenv";
+import pkg from "pg";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
+
+const connection = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "260989",
+  database: "boardcamp",
+});
+
+app.get("/customers", async (req, res) => {
+  try {
+    const customers = await connection.query("SELECT * FROM customers;");
+    res.send(customers.rows);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/customers/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) return res.sendStatus(400)
+
+  try {
+    const customer = await connection.query(
+      `SELECT * FROM customers WHERE id=$1;`,
+      [id]
+    );
+    res.send(customer.rows[0]);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/customers", async (req, res) => {
+
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    await connection.query(
+      "INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)",
+      [name, phone, cpf, birthday]
+    );
+    res.sendStatus(201)
+  } catch (error) {
+    res.send(error)
+  }
+});
